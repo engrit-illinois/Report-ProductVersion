289a1236d36b15c0771e42d4167b0696390b6dd6
mmseng <mmseng@users.noreply.github.com>
2020-12-11 12:20:09 -0600

    moved from gitlab

---
+++ README.md
@@ -0,0 +1,132 @@
+# Summary
+This script takes an SCCM collection name, the name of an application (product), and the filepath of a text file. It polls each computer in the collection and returns a table containing the version of the product installed on each computer. Everything output to the screen is also sent to the log file.  
+
+Must be run on a computer with the SCCM admin console installed if using `-Collection` parameter.  
+
+# Usage
+1. Download `Report-ProductVersion.psm1`
+2. Open a PowerShell console as the user which has SCCM permissions (probably your regular NetID and NOT your SU account).
+3. Import the module: `Import-Module .\Report-ProductVersion.psm1`
+4. Run the command:
+  - e.g. `Report-ProductVersion -Collection "UIUC-ENGR-ESPL" -Product "*acrobat*" -Log "c:\espl-acrobat.log" -Csv "c:\espl-acrobat.csv"`
+  - e.g. `Report-ProductVersion -Computers "gelib-4e-01","eh-406b1-*","mel-1001-01" -Product "*acrobat*"`
+
+# Example
+```powershell
+mseng3@ENGRIT-MMS-RDP C:\> Import-Module .\Report-ProductVersion.psm1
+mseng3@ENGRIT-MMS-RDP C:\> Report-ProductVersion -Collection "UIUC-ENGR-ESPL" -Product "*acrobat*" -Log "c:\epsl-acrobat.log" -Csv "c:\espl-acrobat.csv"
+
+Computers in collection "UIUC-ENGR-ESPL":
+ESPL-114-01
+ESPL-114-02
+ESPL-114-03
+ESPL-114-04
+ESPL-114-05
+ESPL-114-06
+ESPL-114-07
+ESPL-114-08
+ESPL-114-09
+ESPL-MACH-02
+
+Polling computer "ESPL-114-01"...
+    Success. Name(s): "Acrobat DC 19.012.20034 SDL ITP Adobe Acrobat DC Adobe Acrobat Reader DC", Version(s): "1.0.0000 19.012.20034 20.006.20042".
+Polling computer "ESPL-114-02"...
+    Success. Name(s): "Acrobat DC 19.012.20034 SDL ITP Adobe Acrobat DC Adobe Acrobat Reader DC", Version(s): "1.0.0000 19.012.20034 20.009.20063".
+Polling computer "ESPL-114-03"...
+    Success. Name(s): "Acrobat DC 19.012.20034 SDL ITP Adobe Acrobat DC Adobe Acrobat Reader DC", Version(s): "1.0.0000 19.012.20034 20.006.20042".
+Polling computer "ESPL-114-04"...
+    Success. Name(s): "Acrobat DC 19.012.20036 SDL ITP Adobe Acrobat DC Adobe Acrobat Reader DC", Version(s): "1.0.0000 19.012.20036 20.009.20063".
+Polling computer "ESPL-114-05"...
+    Success. Name(s): "Acrobat DC 19.012.20034 SDL ITP Adobe Acrobat DC Adobe Acrobat Reader DC", Version(s): "1.0.0000 19.012.20034 20.006.20042".
+Polling computer "ESPL-114-06"...
+    Success. Name(s): "Acrobat DC 19.012.20034 SDL ITP Adobe Acrobat DC Adobe Acrobat Reader DC", Version(s): "1.0.0000 19.012.20034 20.009.20063".
+Polling computer "ESPL-114-07"...
+    Success. Name(s): "Acrobat DC 19.012.20034 SDL ITP Adobe Acrobat DC Adobe Acrobat Reader DC", Version(s): "1.0.0000 19.012.20034 20.006.20042".
+Polling computer "ESPL-114-08"...
+    Success. Name(s): "Acrobat DC 19.012.20034 SDL ITP Adobe Acrobat DC Adobe Acrobat Reader DC", Version(s): "1.0.0000 19.012.20034 20.009.20063".
+Polling computer "ESPL-114-09"...
+    Unknown COMException!
+Polling computer "ESPL-MACH-02"...
+    Success. Name(s): "Acrobat DC 19.012.20034 SDL ITP Adobe Acrobat DC Adobe Acrobat Reader DC Acrobat", Version(s): "1.0.0000 19.012.20034 20.006.20042 1.0.0000".
+
+PSComputerName Name                            Version
+-------------- ----                            -------
+ESPL-114-01    Adobe Acrobat Reader DC         20.006.20042
+ESPL-114-01    Adobe Acrobat DC                19.012.20034
+ESPL-114-01    Acrobat DC 19.012.20034 SDL ITP 1.0.0000
+ESPL-114-02    Adobe Acrobat Reader DC         20.009.20063
+ESPL-114-02    Adobe Acrobat DC                19.012.20034
+ESPL-114-02    Acrobat DC 19.012.20034 SDL ITP 1.0.0000
+ESPL-114-03    Adobe Acrobat Reader DC         20.006.20042
+ESPL-114-03    Adobe Acrobat DC                19.012.20034
+ESPL-114-03    Acrobat DC 19.012.20034 SDL ITP 1.0.0000
+ESPL-114-04    Adobe Acrobat Reader DC         20.009.20063
+ESPL-114-04    Adobe Acrobat DC                19.012.20036
+ESPL-114-04    Acrobat DC 19.012.20036 SDL ITP 1.0.0000
+ESPL-114-05    Adobe Acrobat Reader DC         20.006.20042
+ESPL-114-05    Adobe Acrobat DC                19.012.20034
+ESPL-114-05    Acrobat DC 19.012.20034 SDL ITP 1.0.0000
+ESPL-114-06    Adobe Acrobat Reader DC         20.009.20063
+ESPL-114-06    Adobe Acrobat DC                19.012.20034
+ESPL-114-06    Acrobat DC 19.012.20034 SDL ITP 1.0.0000
+ESPL-114-07    Adobe Acrobat Reader DC         20.006.20042
+ESPL-114-07    Adobe Acrobat DC                19.012.20034
+ESPL-114-07    Acrobat DC 19.012.20034 SDL ITP 1.0.0000
+ESPL-114-08    Adobe Acrobat Reader DC         20.009.20063
+ESPL-114-08    Adobe Acrobat DC                19.012.20034
+ESPL-114-08    Acrobat DC 19.012.20034 SDL ITP 1.0.0000
+ESPL-114-09    Error                           Error
+ESPL-MACH-02   Adobe Acrobat Reader DC         20.006.20042
+ESPL-MACH-02   Acrobat                         1.0.0000
+ESPL-MACH-02   Acrobat DC 19.012.20034 SDL ITP 1.0.0000
+ESPL-MACH-02   Adobe Acrobat DC                19.012.20034
+
+EOF
+
+mseng3@ENGRIT-MMS-RDP C:\>
+```
+
+# Parameters
+
+### -Computers [string array]
+Required string array, if not using `-Collection`.  
+An array of strings representing either exact computer names, computername queries using `*` wildcards, or a combination thereof.  
+e.g. `Report-ProductVersion -Computers "gelib-4e-01","eh-406b1-*","mel-1001-01" -Product "*acrobat*"`
+
+### -Collection [string]
+Required string.  
+The MECM collection containing the computers you want to query.  
+
+### -Product [string]
+Required string.  
+The name query of the product you want to query for.  
+The script will search for all products whose `Name` or `DisplayName`(depending on the WMI data source) exactly match the given product string. Use `*` as a wildcard.  
+
+### -Log [string]
+Optional string.  
+The full path to a log file where progress will be logged.  
+If omitted, no log will be created.  
+
+### -Csv [string]
+Optional string.  
+The full path to a CSV file where the results will be logged.  
+If omitted, no CSV will be created.  
+
+### -LogIncrementalProgress
+Optional switch.  
+If specified, a table of cumulative result data will be logged each time a computer is polled. Useful in case the script is getting hung up, so you get at least some data.  
+
+### -SiteCode [string]
+Optional string.  
+The SiteCode of the MECM site to use.  
+Default is `MP0`.  
+
+### -Provider [string]
+Optional string.  
+The ProviderMachineName of the MECM server to use.  
+Default is `sccmcas.ad.uillinois.edu`.  
+
+# Notes
+- For large collections, this can take a long time. When machines cannot be contacted it takes 20-25 seconds for the query to timeout, occasionally hanging for much longer. Successful queries may still take a while.
+- To be the most thorough, the script queries WMI for both `Win32_Product` and `Win32Reg_AddRemovePrograms`, as these can both provide distinct sets of results.  
+- By mseng3
---
+++ Report-ProductVersion.psm1
@@ -0,0 +1,230 @@
+# Documentation home: https://github.com/engrit-illinois/Report-ProductVersion
+# By mseng3
+
+function Report-ProductVersion {
+
+	param(
+		# Array or wildcard query of computers to query
+		[Parameter(Position=0,Mandatory=$true,ParameterSetName="Array")]
+		[string[]]$Computers,
+		
+		# Name of collection containing computer to query
+		[Parameter(Position=0,Mandatory=$true,ParameterSetName="Collection")]
+		[string]$Collection,
+		
+		# Product to search for
+		[Parameter(Mandatory=$true)]
+		[string]$Product,
+		
+		# Full path to log file
+		[string]$Log,
+		
+		# Full path to CSV file
+		[string]$Csv,
+		
+		# Logs the full set of results every time a machine is polled
+		# Useful in case things are hanging up, so you get at least some data
+		[switch]$LogIncrementalProgress,
+		
+		# Site code
+		[string]$SiteCode="MP0",
+		
+		# SMS Provider machine name
+		[string]$Provider="sccmcas.ad.uillinois.edu",
+		
+		# ConfigurationManager Powershell module path
+		[string]$CMPSModulePath="$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+
+	)
+
+	function log($msg) {
+		Write-Host $msg
+		if($Log) {
+			Write-Output $msg | Out-File $Log -Append
+		}
+	}
+	
+	# Loads the ConfigMgr Powershell module, and connects to the Site.
+	function Prepare-SCCM {
+		# Customizations
+		$initParams = @{}
+		#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
+		#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
+
+		# Import the ConfigurationManager.psd1 module 
+		if((Get-Module ConfigurationManager) -eq $null) {
+			Import-Module $CMPSModulePath @initParams -Scope Global
+		}
+
+		# Connect to the site's drive if it is not already present
+		if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+			New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $Provider @initParams
+		}
+
+		# Set the current location to be the site code.
+		Set-Location "$($SiteCode):\" @initParams
+	}
+	
+	function Get-CompNameList($compNames) {
+		$list = ""
+		foreach($name in $compNames) {
+			$list = "$list, $name"
+		}
+		$list = $list.Substring(2,$list.length - 2) # Remove leading ", "
+		$list
+	}
+	
+	function Get-CompNames {
+		log "Getting list of computer names..."
+		if($Computers) {
+			log "List was given as an array." -l 1 -v 1
+			$compNames = @()
+			foreach($comp in @($Computers)) {
+				$withoutAsterisk = $comp.Replace("*","")
+				if($withoutAsterisk -eq $comp) {
+					# This comp name has no asterisk, take it at face value
+					$compNames += @($comp)
+				}
+				else {
+					# This comp name has an asterisk, search AD for matching names
+					$compResults = (Get-ADComputer -Filter "Name -like '$comp'" | Select Name).Name
+					foreach($result in @($compResults)) {
+						$compNames += @($result)
+					}
+				}
+			}
+			$list = Get-CompNameList $compNames
+			log "Found $($compNames.count) computers in given array: $list." -l 1
+		}
+		elseif($Collection) {
+			log "List was given as a collection. Getting members of collection: `"$Collection`"..." -l 1 -v 1
+			Prepare-SCCM
+			$colObj = Get-CMCollection -Name $Collection
+			if(!$colObj) {
+				log "The given collection was not found!" -l 1
+			}
+			else {
+				# Get comps
+				$comps = Get-CMCollectionMember -CollectionName $Collection | Select Name,ClientActiveStatus
+				if(!$comps) {
+					log "The given collection is empty!" -l 1
+				}
+				else {
+					# Sort by active status, with active clients first, just in case inactive clients might come online later
+					# Then sort by name, just for funsies
+					$comps = $comps | Sort -Property @{Expression = {$_.ClientActiveStatus}; Descending = $true}, @{Expression = {$_.Name}; Descending = $false}
+					
+					$compNames = $comps.Name
+					$list = Get-CompNameList $compNames
+					log "Found $($compNames.count) computers in `"$Collection`" collection: $list." -l 1
+				}
+			}
+		}
+		else {
+			log "Somehow neither the -Computers, nor -Collection parameter was specified!" -l 1
+		}
+		
+		log "Done getting list of computer names." -v 2
+		
+		$compNames
+	}
+
+	function Get-Data($comps) {
+		# Initialize array
+		$data = @()
+
+		# Attempt to poll each machine
+		foreach($comp in $comps) {
+			log "Polling computer `"$comp`"..."
+			$success = $true
+			$results = "unknown"
+			$productWMI = $Product.Replace("*","%")
+			try {
+				$result1 = Get-WmiObject -Query "select * from win32_product where name like '$productWMI'" -computername $comp -ErrorAction Stop
+				$result2 = Get-WmiObject -Class "Win32Reg_AddRemovePrograms" -Computername $comp -ErrorAction Stop | Where { $_.DisplayName -like $Product }
+			}
+			catch [System.Runtime.InteropServices.COMException] {
+				if($_.Exception.Message.trim() -eq "The RPC server is unavailable.") {
+					log "    Could not contact `"$comp`"!"
+				}
+				else {
+					log "    Unknown COMException!"
+				}
+				$success = $false
+			}
+			catch {
+				log "    Unknown error!"
+				$success = $false
+			}
+			
+			if($success) {
+				$results = @($result1) + @($result2)
+				log "    Success. Found $(@($results).count) matching products."
+			
+				if(@($results).count -ge 1) {
+					foreach($result in @($results)) {
+						$result | Add-Member -NotePropertyName "source" -NotePropertyValue "unknown"
+						if(($result.DisplayName) -and (!($result.Name))) {
+							$result.source = "Win32Reg_AddRemovePrograms"
+							$result | Add-Member -NotePropertyName "Name" -NotePropertyValue $result.DisplayName
+						}
+						elseif(($result.Name) -and (!($result.DisplayName))) {
+							$result.source = "Win32_Product"
+							$result | Add-Member -NotePropertyName "DisplayName" -NotePropertyValue $result.Name
+						}
+						else {
+							log "Found result that had either both, or neither of Name and DisplayName!"
+						}
+						
+						log "        Source: `"$($result.source)`", Name: `"$($result.Name)`", Version: `"$($result.Version)`"."
+					}
+				}
+			}
+			else {
+				$results = @([PSCustomObject]@{
+					"PSComputerName" = $comp
+					"Source" = "Error"
+					"Name" = "Error"
+					"DisplayName" = "Error"
+					"Version" = "Error"
+				})
+			}
+			
+			$data += @($results)
+			
+			#log "    Done."
+			
+			if($LogIncrementalProgress) {
+				log "    $(($data | Out-String).trim())"
+			}
+		}
+		
+		$data
+	}
+
+	function Output-Data($data) {
+		$sdata = $data | Select PSComputerName,Name,Version | Sort PSComputerName
+		
+		# Output data to CSV
+		if($Csv) {
+			$sdata | Export-Csv -Path $Csv -NoTypeInformation -Encoding Ascii
+		}
+		
+		# Output data to log and console
+		log " "
+		log ($sdata | Format-Table -AutoSize -Wrap | Out-String).trim()
+	}
+	
+	
+	$myPWD = $pwd.path
+	$comps = Get-CompNames
+	if($comps -ne $false) {
+		$data = Get-Data $comps
+		Output-Data $data
+	}
+	Set-Location $myPWD
+	
+	log " "
+	log "EOF"
+	log " "
+}
